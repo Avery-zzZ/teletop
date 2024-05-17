6526abe2b82890b0718f6bf8a7996f3964253cea
Avery <3081169057@qq.com>
2024-05-17 22:56:36 +0800

    add feature: init db before run teletop compose

---
+++ init_db.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    from sqlalchemy_utils import database_exists
+    from config import settings
+    
+    if not database_exists(settings.database.url):
+        from sqlalchemy import create_engine
+        from db.dao.base import Base
+        from db.dao.channel import Channel
+        from db.dao.message import Message
+        
+        engine = create_engine(
+            settings.database.url,
+            echo=True
+        )
+        Base.metadata.create_all(engine)
