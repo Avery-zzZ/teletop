f7c0007206bae4ee9c43a5c47b759437639972f3
Avery <3081169057@qq.com>
2024-05-17 23:04:26 +0800

    fix: correct setting usage

--- init_db.py
+++ init_db.py
@@ -2,14 +2,14 @@ if __name__ == "__main__":
     from sqlalchemy_utils import database_exists
     from config import settings
     
-    if not database_exists(settings.database.url):
+    if not database_exists(settings['DATABASE']['url']):
         from sqlalchemy import create_engine
         from db.dao.base import Base
         from db.dao.channel import Channel
         from db.dao.message import Message
         
         engine = create_engine(
-            settings.database.url,
+            settings['DATABASE']['url'],
             echo=True
         )
         Base.metadata.create_all(engine)
